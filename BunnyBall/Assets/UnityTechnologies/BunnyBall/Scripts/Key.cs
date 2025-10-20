c635427a1207538dcd78777b5ed3537b2eaf9957
dddeandre32-dotcom <dddeandre32@gmail.com>
2025-10-20 09:34:10 -0400

    Add comments to code

--- BunnyBall/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
+++ BunnyBall/Assets/UnityTechnologies/BunnyBall/Scripts/Key.cs
@@ -13,9 +13,12 @@ public class Key : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player"))
+            // sets game over to true {
             gameManager.gameOver = true;
+            /creates a shockwave
             Instantiate(shockwavePrefab, transform.position, Quaternion.identity);
+	//destroys key
             Destroy(gameObject, 0.1f);
         }
     }
